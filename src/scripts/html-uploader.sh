a6eb236dbf4d092952e330c010018b9538d218a6
Henrik Herbst Grubbe <hhb@bankdata.dk>
2023-08-21 12:43:41 +0200

    Create folder

--- src/scripts/html-uploader.sh
+++ src/scripts/html-uploader.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-(cd "$HOME/files/html" && exec python3 -m uploadserver --basic-auth fit-user:"$PRINTER_TOKEN")
+path="$HOME/files/html"
+
+mkdir "$path"
+
+(cd "$path" && exec python3 -m uploadserver --basic-auth fit-user:"$PRINTER_TOKEN")
